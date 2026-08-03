b700e1638caf8d890bdab685c5c2f0f9ef895df9
Pranay Nikhare <pranaynikhare7@gmail.com>
2026-08-03 10:58:48 +0530

    add experiment 1 - LR Baseline

---
+++ notebooks/dagshub_setup.py
@@ -0,0 +1,10 @@
+import mlflow
+mlflow.set_tracking_uri('https://dagshub.com/pranaynikhare7/mlops-mini-project.mlflow')
+
+import dagshub
+dagshub.init(repo_owner='pranaynikhare7', repo_name='mlops-mini-project', mlflow=True)
+
+import mlflow
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
